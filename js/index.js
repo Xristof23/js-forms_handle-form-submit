1ec79a47444cf90b288fbbd3651e070f647c86d8
Christof Röhl <userpattern@gmail.com>
2024-01-11 11:32:26 +0100

    solved

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formelements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  const badnessInput = document.querySelector("#badness");
+  const badnessValue =
+    Number(badnessInput.value) + Number(formelements.age.value);
+  const vorName = formelements.firstName.value;
+  console.log("The age-badness-sum of " + vorName + " is " + badnessValue);
+
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
